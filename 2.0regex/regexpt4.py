0e0e3a2a3a23f699d28174e9b5fce9db3fe7c7bc
Alex Yung <superay190@gmail.com>
2022-02-23 22:18:33 -0500

    regex 4 3/10 homecomp commit

---
+++ 2.0regex/regexpt4.py
@@ -0,0 +1,20 @@
+import sys; args = sys.argv[1:]
+idx = int(args[0])-60
+
+
+myRegexLst = [
+  r"/^(?!\d*010)[01]*$/",
+  r"/^(?!\d*010|101)[01]*$/",
+  r"/^([01])[01]*\1$|^[01]$/",
+  r"/\b\w*(\w)(?<!\w*\1)\b/i"
+  
+
+
+  
+  
+ ]
+
+if idx < len(myRegexLst):
+  print(myRegexLst[idx])
+
+  # Alex Yung, 1, 2023
