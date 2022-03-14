ac4b29ff06a1672a748f2a830f20bd0c39acfcd6
bhyun <superay190@gmail.com>
2022-03-14 09:57:26 -0400

    geneticAlgs Progress schoolcomp

--- geneticAlgs.py
+++ geneticAlgs.py
@@ -1,12 +1,20 @@
+from lib2to3.pytree import convert
 import sys
 from  math  import  log
 import string
 import random
-n = int
+n = 0
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 punctuation = string.punctuation
 frequencydict = {}
-usedciphers = ()
+usedciphers = set()
+encodedText = ""
+POPULATION_SIZE = 500
+NUM_CLONES = 1
+TOURNEY_SIZE = 20
+TOURNAMENT_WIN_PROBABILITY = .75
+CROSSOVER_POINTS = 5
+MUTATION_RATE = .8
 
 def getInputFitness():
     global n
@@ -36,6 +44,18 @@ def getInputHillDebug():
     # encodedtext = "XMTP  CGPQR  BWEKNJB  GQ  OTGRB  EL  BEQX  BWEKNJB,  G  RFGLI.   GR  GQ  BEQX ABSETQB  RFGQ  QBLRBLSB  TQBQ  EJJ  RBL  KMQR  SMKKML  VMPYQ  GL  BLDJGQF: G  FEUB  RM  AB  E  DMMY  QRTYBLR  GL  RFER  SJEQQ  GL  RFB  PMMK  MC  RFER RBESFBP.'".lower()
     encodedtext = "PF HACYHTTRQ VF N PBYYRPGVBA BS SERR YRNEAVAT NPGVIVGVRF GUNG GRNPU PBZCHGRE FPVRAPR GUEBHTU RATNTVAT TNZRF NAQ CHMMYRF GUNG HFR PNEQF, FGEVAT, PENLBAF NAQ YBGF BS EHAAVAT NEBHAQ. JR BEVTVANYYL QRIRYBCRQ GUVF FB GUNG LBHAT FGHQRAGF PBHYQ QVIR URNQ- SVEFG VAGB PBZCHGRE FPVRAPR, RKCREVRAPVAT GUR XVAQF BS DHRFGVBAF NAQ PUNYYRATRF GUNG PBZCHGRE FPVRAGVFGF RKCREVRAPR, OHG JVGUBHG UNIVAT GB YRNEA CEBTENZZVAT SVEFG. GUR PBYYRPGVBA JNF BEVTVANYYL VAGRAQRQ NF N ERFBHEPR SBE BHGERNPU NAQ RKGRAFVBA, OHG JVGU GUR NQBCGVBA BS PBZCHGVAT NAQ PBZCHGNGVBANY GUVAXVAT VAGB ZNAL PYNFFEBBZF NEBHAQ GUR JBEYQ, VG VF ABJ JVQRYL HFRQ SBE GRNPUVAT. GUR ZNGREVNY UNF ORRA HFRQ VA ZNAL PBAGRKGF BHGFVQR GUR PYNFFEBBZ NF JRYY, VAPYHQVAT FPVRAPR FUBJF, GNYXF SBE FRAVBE PVGVMRAF, NAQ FCRPVNY RIRAGF. GUNAXF GB TRAREBHF FCBAFBEFUVCF JR UNIR ORRA NOYR GB PERNGR NFFBPVNGRQ ERFBHEPRF FHPU NF GUR IVQRBF, JUVPU NER VAGRAQRQ GB URYC GRNPUREF FRR UBJ GUR NPGVIVGVRF JBEX (CYRNFR QBA’G FUBJ GURZ GB LBHE PYNFFRF – YRG GURZ RKCREVRAPR GUR NPGVIVGVRF GURZFRYIRF!). NYY BS GUR NPGVIVGVRF GUNG JR CEBIVQR NER BCRA FBHEPR – GURL NER ERYRNFRQ HAQRE N PERNGVIR PBZZBAF NGGEVOHGVBA-FUNERNYVXR YVPRAPR, FB LBH PNA PBCL, FUNER NAQ ZBQVS GUR ZNGREVNY. SBE NA RKCYNANGVBA BA GUR PBAARPGVBAF ORGJRRA PF HACYHTTRQ NAQ PBZCHGNGVBANY GUVAXVAT FXVYYF, FRR BHE PBZCHGNGVBANY GUVAXVAT NAQ PF HACYHTTRQ CNTR. GB IVRJ GUR GRNZ BS PBAGEVOHGBEF JUB JBEX BA GUVF CEBWRPG, FRR BHE CRBCYR CNTR. SBE QRGNVYF BA UBJ GB PBAGNPG HF, FRR BHE PBAGNPG HF CNTR. SBE ZBER VASBEZNGVBA NOBHG GUR CEVAPVCYRF ORUVAQ PF HACYHTTRQ, FRR BHE CEVAPVCYRF CNTR.".lower()
     return encodedtext
+
+def getInputGenAlg():
+    global n, encodedText
+    n = sys.argv[1]
+    encodedText = sys.argv[2]
+    return encodedText
+
+def getInputGenAlgDebug():
+    global n, encodedText
+    n = 3
+    encodedText = "PF HACYHTTRQ VF N PBYYRPGVBA BS SERR YRNEAVAT NPGVIVGVRF GUNG GRNPU PBZCHGRE FPVRAPR GUEBHTU RATNTVAT TNZRF NAQ CHMMYRF GUNG HFR PNEQF, FGEVAT, PENLBAF NAQ YBGF BS EHAAVAT NEBHAQ. JR BEVTVANYYL QRIRYBCRQ GUVF FB GUNG LBHAT FGHQRAGF PBHYQ QVIR URNQ- SVEFG VAGB PBZCHGRE FPVRAPR, RKCREVRAPVAT GUR XVAQF BS DHRFGVBAF NAQ PUNYYRATRF GUNG PBZCHGRE FPVRAGVFGF RKCREVRAPR, OHG JVGUBHG UNIVAT GB YRNEA CEBTENZZVAT SVEFG. GUR PBYYRPGVBA JNF BEVTVANYYL VAGRAQRQ NF N ERFBHEPR SBE BHGERNPU NAQ RKGRAFVBA, OHG JVGU GUR NQBCGVBA BS PBZCHGVAT NAQ PBZCHGNGVBANY GUVAXVAT VAGB ZNAL PYNFFEBBZF NEBHAQ GUR JBEYQ, VG VF ABJ JVQRYL HFRQ SBE GRNPUVAT. GUR ZNGREVNY UNF ORRA HFRQ VA ZNAL PBAGRKGF BHGFVQR GUR PYNFFEBBZ NF JRYY, VAPYHQVAT FPVRAPR FUBJF, GNYXF SBE FRAVBE PVGVMRAF, NAQ FCRPVNY RIRAGF. GUNAXF GB TRAREBHF FCBAFBEFUVCF JR UNIR ORRA NOYR GB PERNGR NFFBPVNGRQ ERFBHEPRF FHPU NF GUR IVQRBF, JUVPU NER VAGRAQRQ GB URYC GRNPUREF FRR UBJ GUR NPGVIVGVRF JBEX (CYRNFR QBA’G FUBJ GURZ GB LBHE PYNFFRF – YRG GURZ RKCREVRAPR GUR NPGVIVGVRF GURZFRYIRF!). NYY BS GUR NPGVIVGVRF GUNG JR CEBIVQR NER BCRA FBHEPR – GURL NER ERYRNFRQ HAQRE N PERNGVIR PBZZBAF NGGEVOHGVBA-FUNERNYVXR YVPRAPR, FB LBH PNA PBCL, FUNER NAQ ZBQVS GUR ZNGREVNY. SBE NA RKCYNANGVBA BA GUR PBAARPGVBAF ORGJRRA PF HACYHTTRQ NAQ PBZCHGNGVBANY GUVAXVAT FXVYYF, FRR BHE PBZCHGNGVBANY GUVAXVAT NAQ PF HACYHTTRQ CNTR. GB IVRJ GUR GRNZ BS PBAGEVOHGBEF JUB JBEX BA GUVF CEBWRPG, FRR BHE CRBCYR CNTR. SBE QRGNVYF BA UBJ GB PBAGNPG HF, FRR BHE PBAGNPG HF CNTR. SBE ZBER VASBEZNGVBA NOBHG GUR CEVAPVCYRF ORUVAQ PF HACYHTTRQ, FRR BHE CEVAPVCYRF CNTR.".lower()
+    return encodedText
 
 def fillFreqDict():
     with open("ngrams.txt", "r") as f:
@@ -94,6 +114,12 @@ def fitness(texttype, etext, cipher):
     calc = calcumalations(grams)
     return calc
 
+def fitnessSpecialized(cipher):
+    dtext = decodeInit(encodedText, cipher)
+    grams = getNGrams(dtext)
+    calc = calcumalations(grams)
+    return calc
+
 def getRandomCipher():
     temp = alphabet
     temp = ''.join(random.sample(temp, len(temp)))
@@ -104,7 +130,7 @@ def hillClimb():
     count = 0
     cipher = getRandomCipher()
     best = hillClimbHelper(etext, cipher)
-    while(count < 5000):
+    while(count < 1000):
         tempcipher = swap(cipher)
         newtext = hillClimbHelper(etext, tempcipher)
         if(newtext[0] > best[0]):
@@ -141,6 +167,13 @@ def swap(text):
     toReturn = toReturn[0:randint2] + temp1 + toReturn[randint2+1:]
     return toReturn
 
+def convertWithScores(listOfCiphers):
+    toReturn = []
+    for i in listOfCiphers:
+        fitscore = fitnessSpecialized(i)
+        toReturn.append([fitscore, i])
+    return toReturn
+
 def population(n):
     global usedciphers
     toReturn = []
@@ -148,15 +181,113 @@ def population(n):
         temp = getRandomCipher()
         while(temp in usedciphers):
             temp = getRandomCipher()
-        usedciphers = usedciphers + (temp)
+        usedciphers.add(temp)
         toReturn.append(temp)
     return toReturn
 
-def selection():
-    return 0
+def selection(orderedOrigin):
+    nextGen = []
+    #add some top current gen to nextgen
+    orderedOrigin.sort(reverse = True)
+    for i in range(0, NUM_CLONES):
+        nextGen.append(orderedOrigin[i])
+    
+    tourney = random.sample(orderedOrigin, TOURNEY_SIZE*2)
+    t1 = tourney[:len(tourney)//2]
+    t2 = tourney[len(tourney)//2:]
+    t1.sort(reverse = True)
+    t2.sort(reverse = True)
+    parents = []
+    for i in t1:
+        if(random.random() < TOURNAMENT_WIN_PROBABILITY):
+            parents.append(i)
+            break
 
-def breeding():
-    return 0
+    for i in t2:
+        if(random.random() < TOURNAMENT_WIN_PROBABILITY):
+            parents.append(i)
+            break
+    
+    return parents
+
+
+
+def breeding(parents):
+    p1 = parents[0]
+    p2 = parents[1]
+    child = "--------------------------"
+    crossovers = random.sample(p1[1], CROSSOVER_POINTS)
+    for i in crossovers:
+        tempindex = p1[1].index(i)
+        child = child[0:tempindex] + i + child[tempindex+1:]
+    
+    index = 0
+    for i in p2[1]:
+        if(i not in child):
+            while(child[index:index+1] != "-"):
+                index+=1
+            child = child[0:index] + i + child[index+1:]
+    
+    return child
+
+def mutation(child):
+    global usedciphers
+    if(random.random() < MUTATION_RATE):
+        toReturn = child
+        range = len(toReturn)-1
+        randint1 = random.randint(0, range)
+        randint2 = random.randint(0, range)
+        while(randint2 == randint1):
+            randint2 = random.randrange(0, range)
+        temp1 = toReturn[randint1:randint1+1]
+        toReturn = toReturn[0:randint1] + toReturn[randint2:randint2+1]+ toReturn[randint1+1:]
+        toReturn = toReturn[0:randint2] + temp1 + toReturn[randint2+1:]
+        usedciphers.add(toReturn)
+        if(toReturn in usedciphers):
+            return mutation(child)
+        return toReturn
+    else:
+        return child
+
+def getInitChildren(n):
+    toReturn = []
+    for i in range(n):
+        pop = population(TOURNEY_SIZE*2)
+        pop = convertWithScores(pop)
+        par = selection(pop)
+        child = breeding(par)
+        mutantchild = mutation(child)
+        toReturn.append(mutantchild)
+    return toReturn
+
+def genAlg(etext):
+    count = 1
+    cInit = getInitChildren(300)
+    cScoresInit = convertWithScores(cInit)
+    cScoresInit.sort(reverse = True)
+    print(cScoresInit)
+    bestcipher = cScoresInit[0][1]
+    print(bestcipher)
+
+    while(count < 500):
+        newgen = []
+        for i in range(300):
+            newSel = selection(cScoresInit)
+            newChild = breeding(newSel)
+            newMutChild = mutation(newChild)
+            newgen.append(newMutChild)
+
+        cScoresInit = newgen
+        cScoresInit = convertWithScores(cScoresInit)
+        cScoresInit.sort()
+        print(cScoresInit[0])
+        count+=1
+
+    
+
+
+
+
 
 fillFreqDict()
 
@@ -176,7 +307,8 @@ fillFreqDict()
 # print()
 
 #Genetic Alg
-
+etext = getInputGenAlgDebug()
+thing = genAlg(etext)
 
 
 
