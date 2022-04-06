70ab508427d61fe298d7fd298479fbcc0a91060c
Alex Yung <superay190@gmail.com>
2022-04-05 21:43:14 -0400

    tetris pt 1 complete

--- tetris.py
+++ tetris.py
@@ -9,7 +9,6 @@ types = {"I0": " " * 12 + "####", "I1": "#" + "   " + "#" + "   " + "#" + "   " 
          "Z0": " " * 8 + "##  " + " ## ", "Z1": " " * 4 + " #  " + "##  " + "#   ", 
          "J0": " " * 8 + "#   " + "### ", "J1": " " * 4 + "##  " + "#   " + "#   ", "J2": " " * 8 + "### " + "  # ", "J3": " " * 4 + " #  " + " #  " + "##  ",
          "L0": " " * 8 + "  # " + "### ", "L1": " " * 4 + "#   " + "#   " + "##  ", "L2": " " * 8 + "### " + "#   ", "L3": " " * 4 + "##  " + " #  " + " #  "}
-colHeights = []
 lowestpoints = {}
 
 def input():
@@ -50,23 +49,29 @@ def printPiece(i):
             print(types[i][x-1:x], end = "")
 
 def getColHeights(board):
-    global colHeights
-    colHeights = []
+    cH = []
     for w in range(10):
         for h in range(20):
             index = w + (h*10)
             if(board[index:index+1] == "#"):
-                colHeights.append(20-h)
+                cH.append(20-h)
                 break
-    return colHeights
+    return cH
         
 
-def output(strboard):
+def output(board):
     with open("tetrisout.txt", "a") as r:
-        r.write(printFancyBoard(strboard))
+        for i in types:
+            colHeights = getColHeights(board)
+            powsibilities = addPiece(board, i, colHeights)
+            for b in powsibilities:
+                if(b == None):
+                    r.write("GAME OVER\n")
+                else:
+                    r.write(b + "\n")
 
 
-def getHeight(piece):
+def getHeight(piece): #returns maxblockH, then other heights relative
     block = types[piece]
     maxblockH = 0
     for x in range(4):
@@ -92,32 +97,135 @@ def getHeight(piece):
     
     return heightList
 
-def addPiece(board, piece):
+def getLHeight(piece):
+    block = types[piece]
+    count = 0
+    start = 0
+    startreached = False
+    for h in reversed(range(4)):
+        index = (h*4)
+        if(block[index:index+1] == "#"):
+            count+=1
+            if(startreached == False):
+                start = 4-h-1
+                startreached = True
+    return [count, start]
+
+def getTop(piece, startpoint):
+    block = types[piece]
+    tops = []
+    # leftheight = 4 - leftmostheight
+    for w in range(1, 4):
+        temptop = 0
+        for h in range(4):
+            index = w + (h*4)
+            if(block[index:index+1] == "#"):
+                temptop = 4 - h - (startpoint+1)
+                break
+        tops.append(temptop)
+    return tops
+
+
+def addPiece(board, piece, colHeights):
     pieceheights = getHeight(piece)
     indexes = []
     thiccc = 10 - (len(pieceheights)-2)
+    pain = getLHeight(piece)
+    leftmostheight = pain[0]
+    startpoint = pain[1]
+    tops = getTop(piece, startpoint)
     for w in range(thiccc):
-        if(pieceheights[0] + colHeights[w] > 20):
-            indexes.append(-1)
-        else:
+        # if(max(tops) + leftmostheight + colHeights[w]> 20):
+        #     indexes.append(-1)
+        # else:
+        if(len(pieceheights) < 3):
             tempind = colHeights[w]+1
-            for h in range(1, len(pieceheights)-1):
-                while(colHeights[w+h] - tempind >= pieceheights[h]):
-                    tempind+=1
+            if(tempind + pieceheights[0] -1 > 20):
+                indexes.append(-1)
+            else:
                 indexes.append(tempind)
+        else:
+            tempind = colHeights[w]-1
+            for h in range(2, len(pieceheights)):
+                newCand = 0
+                if(pieceheights[1] < pieceheights[h]):
+                    if(colHeights[w+h-1] > colHeights[w]):
+                        if(colHeights[w] - colHeights[w+h-1] <= (pieceheights[h]* -1)):
+                            newCand = colHeights[w+h-1] + 1 - pieceheights[h]
+                        else:
+                            newCand = colHeights[w+h-1]
+                    else:
+                        newCand = colHeights[w] + 1
+                elif(pieceheights[1] > pieceheights[h]):
+                    if(colHeights[w] > colHeights[w+h-1]):
+                        if(colHeights[w] - colHeights[w+h-1] >= (pieceheights[h]*-1)):
+                            newCand = colHeights[w] + 1 - (pieceheights[h]*-1)
+                        else:
+                            newCand = colHeights[w+h-1]
+                    else:
+                        newCand = colHeights[w+h-1] + 1
+                else:
+                    newCand = max(colHeights[w+h-1], colHeights[w]) + 1 - startpoint
+                if(newCand > tempind):
+                    tempind = newCand
+            indexes.append(tempind)
     print(indexes)
+    boardsList = []
+    for i in range(len(indexes)):
+        tempboard = placePieces(board, indexes[i], i, piece)
+        # printFancyBoard(tempboard)
+        toReturnBoard = tempboard
+        if(tempboard != "GAME OVER" and tempboard != None):
+            toReturnBoard = removeLines(tempboard)
+        # printFancyBoard(toReturnBoard)
+        boardsList.append(toReturnBoard)
+    return boardsList
+
+def placePieces(board, indexR, indexC, piece):
+    initR = 4
+    initC = 1
+    toReturnBoard = board
+    piecestr = types[piece]
+    if(indexR != -1):
+        bigR = 20 - indexR
+        boardPivInd = (bigR * 10) + indexC
+        for r in reversed(range(4)):
+            #use the bottom left corner as the pivot and go from there
+            #go by row, botom to top.
+            if(boardPivInd < 0 and "#" in piecestr[4*r:4*r + 3]):
+                toReturnBoard = "GAME OVER"
+                break
+            else:
+                for c in range(4):
+                    piecePivInd = 4 * r
+                    if(piecestr[piecePivInd+c:piecePivInd+c+1] == "#"):
+                        toReturnBoard = toReturnBoard[:boardPivInd+c] + piecestr[piecePivInd+c:piecePivInd+c+1] + toReturnBoard[boardPivInd+c+1:]
+                    # [boardPivInd+c:boardPivInd+c+1]
+            boardPivInd -= 10
+        return toReturnBoard
+    else:
+        return None
+
+def removeLines(board):
+    newboard = board
+    for r in range(0, len(newboard), 10):
+        if(newboard[r:r+10] == "##########"):
+            newboard = " " * 10 + newboard[0:r] + newboard[r+10:]
+            r-=10
+    return newboard
 
 
 
-            
-            
-            
 
 
 strboard = input()
-printFancyBoard(strboard)
-# printTypes()
-print(getColHeights(strboard))
-printPiece("S1")
-print(getHeight("S1"))
-addPiece(strboard, "S1")
+
+
+# printFancyBoard(strboard)
+# colHeights = getColHeights(strboard)
+# printPiece("L0")
+# powsibilities = addPiece(strboard, "L0", colHeights)
+# for board in powsibilities:
+#         printFancyBoard(board)
+
+output(strboard)
